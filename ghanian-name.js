f58bc9e0defc2cd688e5453a34cdd7e9a31eb268
Alvin Awuor <alvinawuor8@gmail.com>
2021-02-20 02:08:26 +0300

    Add const variables for day of week formula.

--- ghanian-name.js
+++ ghanian-name.js
@@ -7,6 +7,15 @@ function collectData() {
     var formData = new FormData(form[0]);
     alert(formData.get('first_name') + " " + formData.get('last_name') + " " + formData.get('gender') + " " + formData.get('year') + " " + formData.get('month') + " " + formData.get('day'));
 
+    const century = parseInt(formData.get('year').slice(0, 2));
+    const year = parseInt(formData.get('year').slice(2));
+    const month = parseInt(formData.get('month'));
+    const day = parseInt(formData.get('day'));
+
+    if(century === 23) {
+        alert("yes");
+    }
+
     document.forms[0].reset();
 }
 
