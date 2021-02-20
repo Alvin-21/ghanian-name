d92161866bf6798a8af4c220653240866e883f16
Alvin Awuor <alvinawuor8@gmail.com>
2021-02-20 03:32:35 +0300

    Add JavaScript code for day of week formula.

--- ghanian-name.js
+++ ghanian-name.js
@@ -8,14 +8,31 @@ function collectData() {
     alert(formData.get('first_name') + " " + formData.get('last_name') + " " + formData.get('gender') + " " + formData.get('year') + " " + formData.get('month') + " " + formData.get('day'));
 
     const century = parseInt(formData.get('year').slice(0, 2));
-    const year = parseInt(formData.get('year').slice(2));
-    const month = parseInt(formData.get('month'));
+    let year = parseInt(formData.get('year').slice(2));
+    let month = parseInt(formData.get('month'));
     const day = parseInt(formData.get('day'));
 
     if(century === 23) {
         alert("yes");
     }
 
+    function dayOfWeek() {
+        if(month <= 2) {
+            month += 10;
+        } else {
+            month -= 2;
+        }
+
+        if(month === 11 || month === 12) {
+            year -= 1;
+        }
+        
+        let dayOfWeek = (day + Math.floor((2.6 * month) - 0.2) - (2 * century) + year + Math.floor(year / 4) + Math.floor(century / 4)) % 7;
+        
+        return dayOfWeek;
+    }
+
+    alert(dayOfWeek());
     document.forms[0].reset();
 }
 
