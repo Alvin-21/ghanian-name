56e3e631272f800f74987c9b9e4bf95b3deab41d
Alvin Awuor <alvinawuor8@gmail.com>
2021-02-22 11:26:15 +0300

    Add error message for incorrect date.

--- ghanian-name.js
+++ ghanian-name.js
@@ -14,17 +14,18 @@ function collectData() {
 
     let index;
     function dayOfWeek() {
+        let calculatedMonth;
         if(month <= 2) {
-            month += 10;
+            calculatedMonth = month + 10;
         } else {
-            month -= 2;
+            calculatedMonth = month - 2;
         }
 
         if(month === 11 || month === 12) {
             year -= 1;
         }
 
-        let weekDay = (day + Math.floor((2.6 * month) - 0.2) - (2 * century) + year + Math.floor(year / 4) + Math.floor(century / 4)) % 7;
+        let weekDay = (day + Math.floor((2.6 * calculatedMonth) - 0.2) - (2 * century) + year + Math.floor(year / 4) + Math.floor(century / 4)) % 7;
 
         if(weekDay < 0) {
             weekDay += 7;
@@ -38,10 +39,17 @@ function collectData() {
     const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+    let name;
     if(gender === "male") {
-        alert("Your Ghanian Name is: " + maleNames[index]);
+        name = maleNames[index];
     } else {
-        alert("Your Ghanian Name is: " + femaleNames[index]);
+       name = femaleNames[index];
+    }
+
+    if(month < 1 || month > 12 || day < 1 || day > 31) {
+        alert("Please make sure you have entered the correct date.\nDay should be between 1-31 and Month should be between 1-12.");
+    } else {
+        alert(name);
     }
 
     document.forms[0].reset();
